28cd4f1e5c32900facf636a8f3cf5c3361c886e3
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2021-09-14 22:01:32 +0900

    카드 스타일링

--- client/src/components/CardList/style.tsx
+++ client/src/components/CardList/style.tsx
@@ -2,6 +2,31 @@ import styled from 'styled-components';
 
 export const CardList = styled.div``;
 
-export const Card = styled.div``;
+export const Card = styled.div`
+  width: 300px;
+  padding: 1rem;
+  box-shadow: 0px 1px 30px rgba(224, 224, 224, 0.3);
+  border-radius: 6px;
+  margin-bottom: 1rem;
+
+  & > div:not(:last-child) {
+    margin-bottom: 0.5rem;
+  }
+
+  .card-title {
+    font-weight: 600;
+    font-size: 1.25rem;
+  }
+
+  .card-expect-time,
+  .card-proceed-time {
+    display: flex;
+    font-size: 0.875rem;
+    & > div:first-child {
+      font-weight: 600;
+      margin-right: 0.5rem;
+    }
+  }
+`;
 
 export const CardEditForm = styled.div``;
