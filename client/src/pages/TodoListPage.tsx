e25d9f0ce4039450fea8cded91a24f45cdb7fe05
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2021-09-16 22:27:50 +0900

    투두 리스트 페이지 컴포넌트 제목 스타일링

--- client/src/pages/TodoListPage.tsx
+++ client/src/pages/TodoListPage.tsx
@@ -7,9 +7,16 @@ interface Props {}
 export default function TodoListPage({}: Props): ReactElement {
   return (
     <StyledTodoListPage>
+      <div className='todo-page-title'>TODO LIST</div>
       <CardList />
     </StyledTodoListPage>
   );
 }
 
-const StyledTodoListPage = styled.div``;
+const StyledTodoListPage = styled.div`
+  .todo-page-title {
+    font-size: 2rem;
+    font-weight: 600;
+    margin-bottom: 2rem;
+  }
+`;
